49e7ec27b50922a86021092d5a9527657d1b6516
kalundaah <84059634+kalundaah@users.noreply.github.com>
2024-02-23 15:25:50 +0300

    Created client side routing

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigationpane from '../components/nav'
+import Content from '../components/homepagecontent'
+import { Outlet,link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <Content/>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/signinup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet,link } from 'react-router-dom';
+function Signinup() {
+  return (
+    <div>
+      Signinup
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Signinup
